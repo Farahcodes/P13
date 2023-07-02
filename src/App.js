ba0bbeaeea98f5fe14d56adbaa0934362886f0fb
Farah Fournat <farah.fournat@gmail.com>
2023-07-02 14:24:04 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Provider } from 'react-redux';
+import { userStore } from './redux/userStore';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={userStore}>
+      <Header />
+      <Outlet />
+      <Footer />
+    </Provider>
   );
 }
-
-export default App;
