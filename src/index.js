afc4b5877361fce560f099a2f0672d15183893ab
Farah Fournat <farah.fournat@gmail.com>
2023-07-02 14:23:27 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from './App';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import User from './pages/User';
+
+const container = document.getElementById('root'),
+  root = ReactDOM.createRoot(container);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/profile" element={<User />} />
+          <Route path="*" element={<>404</>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
