577069efcaf21aca6d07eaae28c25f671325088c
Farah Fournat <farah.fournat@gmail.com>
2023-07-27 10:42:16 +0200

    added documentation for login(email, password)

--- src/api/login.js
+++ src/api/login.js
@@ -1,15 +1,24 @@
+/**
+ * This function authenticates the user against the backend API.
+ *
+ * @param {string} email - The email of the user trying to log in.
+ * @param {string} password - The password of the user trying to log in.
+ *
+ * @returns {Promise} A Promise that resolves to the server's response as a JavaScript object.
+ */
 export const login = (email, password) =>
   fetch(process.env.REACT_APP_BACKEND_API + 'user/login', {
-    method: 'POST',
+    method: 'POST', // HTTP method
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
+      // HTTP headers
+      Accept: 'application/json', // informs the server about the types of data that can be sent back
+      'Content-Type': 'application/json', // informs the server about the type of data being sent (in the body)
     },
-    body: JSON.stringify({ email, password }),
+    body: JSON.stringify({ email, password }), // body data type must match 'Content-Type' header
   })
-    .then((response) => response.json())
+    .then((response) => response.json()) // parses JSON response into native JavaScript objects
     .then((data) => {
-      return data;
+      return data; // returns data received from the server
     });
 
 export const getData = (token) =>
