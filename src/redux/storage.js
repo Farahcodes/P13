5c8249f059d0d6241939baa514ec425950eb3129
Farah Fournat <farah.fournat@gmail.com>
2023-08-26 20:14:50 +0200

    added documentation for loadState function

--- src/redux/storage.js
+++ src/redux/storage.js
@@ -8,11 +8,26 @@
  * - `sessionStorage`: stores data for one session (data is lost when the browser tab is closed).
  */
 
+/**
+ * Load application state from web storage.
+ *
+ * This function first tries to retrieve the state from `sessionStorage`.
+ * If it doesn't find any state there, it checks in `localStorage`.
+ * If it doesn't find the state in both storage options, it returns an empty object.
+ *
+ * @returns {Object} The stored state if found, otherwise an empty object.
+ */
 export const loadState = () => {
+  // Check if there's a state saved in sessionStorage.
   if (sessionStorage.getItem('state') !== null)
+    // If found, parse and return the state.
     return JSON.parse(sessionStorage.getItem('state'));
+
+  // If the state wasn't found in sessionStorage, check in localStorage.
   if (localStorage.getItem('state') !== null)
+    // If found, parse and return the state.
     return JSON.parse(localStorage.getItem('state'));
+  // If no state is found in both storages, return an empty object.
   return {};
 };
 
