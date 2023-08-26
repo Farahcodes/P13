d3e17dbc0f232718094195f6638fe4ffc40ff243
Farah Fournat <farah.fournat@gmail.com>
2023-08-26 20:02:44 +0200

    difference between localStorage and sessionStorage

--- src/redux/storage.js
+++ src/redux/storage.js
@@ -1,4 +1,13 @@
-// Load state from sessionStorage or localStorage, else return an empty Object
+/**
+ * The `loadState` and `saveState` functions are utilities for working with
+ * browser's Web Storage (specifically `localStorage` and `sessionStorage`).
+ * These storage options allows to store key/value pairs in a web browser.
+ *
+ * The main difference between `localStorage` and `sessionStorage`:
+ * - `localStorage`: stores data with no expiration time.
+ * - `sessionStorage`: stores data for one session (data is lost when the browser tab is closed).
+ */
+
 export const loadState = () => {
   if (sessionStorage.getItem('state') !== null)
     return JSON.parse(sessionStorage.getItem('state'));
