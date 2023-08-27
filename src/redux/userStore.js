b15efce643d1e43bbe469ba0da1d6a8357016d53
Farah Fournat <farah.fournat@gmail.com>
2023-08-27 19:56:26 +0200

    defining Redux actions types

--- src/redux/userStore.js
+++ src/redux/userStore.js
@@ -2,10 +2,10 @@ import { createStore } from 'redux';
 import { loadState } from './storage';
 
 // Actions type
-const LOGIN_VALID = 'LOGIN_VALID',
-  LOGOUT = 'LOGOUT',
-  SAVE_STORAGE = 'SAVE_STORAGE',
-  PROFILE_UPDATE = 'PROFILE_UPDATE';
+const LOGIN_VALID = 'LOGIN_VALID', // Triggered when a user logs in successfully.
+  LOGOUT = 'LOGOUT', // Triggered when a user logs out.
+  SAVE_STORAGE = 'SAVE_STORAGE', // Used to save some data related to the storage mechanism.
+  PROFILE_UPDATE = 'PROFILE_UPDATE'; // Triggered when a user updates their profile.
 
 // Reducer
 const userReducer = (state, action) => {
